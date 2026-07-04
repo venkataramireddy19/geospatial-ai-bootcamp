d7dfcb3453ab2aa97d5dc9bd3cdfdece2c96fb04
Venkata Ramireddy Pesala <pvramireddy19@gmail.com>
2026-07-04 19:17:56 +0530

    Initial project setup

---
+++ hello_geospatial.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import platform
+import os
+
+print("Name : Venkata Ramireddy Pesala")
+print("Welcome to Geospatial AI Bootcamp")
+
+print("Current Date & Time:")
+print(datetime.now())
+
+print("\nPython Version:")
+print(platform.python_version())
+
+print("\nWorking Directory:")
+print(os.getcwd())
